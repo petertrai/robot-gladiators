594c22e6d2d575ddffae59af6583ac2c5a4e92f0
Petertrai <petertrai@gmail.com>
2023-04-26 21:03:32 -0400

    more java script

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,7 +18,10 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-
+     
+    //repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+ 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -68,10 +71,14 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
-}; // end of fight function
+}
+}
+// end of fight function
 
 // run fight function to start game
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
